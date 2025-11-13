385a6373bc6494d048d835235aef04b8fdcef27f
Franco Barreto <franco.barreto@gmail.com>
2025-11-13 12:31:11 -0300

    initial commit

---
+++ aulanet/aulanet/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for aulanet project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aulanet.settings')
+
+application = get_asgi_application()
