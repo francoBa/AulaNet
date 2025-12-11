69b917dc8f387c535e0327a40beaa8b6b632b751
AdCh571 <lic.adchavez@gmail.com>
2025-12-11 17:40:43 -0300

    configuracion basica de CORE admin.py y models.py

--- aulanet/apps/core/admin.py
+++ aulanet/apps/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject', 'created_at')
+    search_fields = ('name', 'email', 'subject')
+
+admin.site.register(Contact, ContactAdmin)
+
