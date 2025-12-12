a8ae8931a76a03d997b631e27a6ece4e1aa1ecdb
Martin Emanuel Romero <martinromeroutn@gmail.com>
2025-12-12 11:50:11 -0300

    feat(school): add slug field to School model and update related views and templates

--- aulanet/apps/school/models.py
+++ aulanet/apps/school/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.conf import settings
+from django.utils.text import slugify
 
 import uuid
 
@@ -17,7 +18,8 @@ class School(models.Model):
     ]
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
     school_type = models.CharField(max_length=20, choices=SCHOOL_TYPES)
     school_level = models.CharField(max_length=20, choices=SCHOOL_LEVEL)
     city = models.CharField(max_length=120)
@@ -27,6 +29,21 @@ class School(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.name)
+            slug = base_slug
+            counter = 1
+
+            # asegurar que el slug sea único
+            while School.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+
+            self.slug = slug
+
+        super().save(*args, **kwargs)
 
 
 
